95717d160f21e9016ab43a04807ce2bd8c17af19
Diego Marzo <diegomarzo@gmail.com>
2020-09-18 14:46:13 +0200

    Printing a big badass error syntax message

--- src/main/kotlin/io/suprgames/serverless/ServerlessDSLGeneratorMain.kt
+++ src/main/kotlin/io/suprgames/serverless/ServerlessDSLGeneratorMain.kt
@@ -1,16 +1,32 @@
 package io.suprgames.serverless
 
 import io.suprgames.serverless.generator.ServerlessKDSLGenerator
+import java.lang.RuntimeException
 
 object ServerlessDSLGeneratorMain {
 
     @JvmStatic
     fun main(vararg args: String) {
-        println(args)
         if (args.size == 3) {
+            println("Arguments provided: " + args.asList())
             ServerlessKDSLGenerator(args[0], args[1], args[2])
         } else {
-            ServerlessKDSLGenerator()
+            println(" ************************************************************************************")
+            println("   Syntax error, arguments number must be 3 and there are ${args.size}: ")
+            println()
+            println(" Provided: ${args.asList()}")
+            println()
+            println("   Three values are required: ")
+            println("   - 1) baseFile:       Base file that will used to generate the serverless file from")
+            println("   - 2) basePackage:    Base package that will be used to do the reflection")
+            println("   - 3) serverlessFile: Serverless result file")
+            println()
+            println("   Argument list example: ")
+            println("""    listOf("serverless-base.yml", "com.wizanit", "serverless.yml")""")
+            println(" ************************************************************************************")
+            println()
+            println()
+            throw RuntimeException("Invalid number of parameters provided ${args.size}, must be 3")
         }.generate()
     }
 
