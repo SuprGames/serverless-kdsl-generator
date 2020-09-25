890d5b4853c84dc20db4b22fb3bc0143c0d37104
Diego Marzo <diegomarzo@gmail.com>
2020-09-25 09:49:34 +0200

    AlterEgo support for general functions

--- src/main/kotlin/io/suprgames/serverless/generator/ServerlessKDSLGenerator.kt
+++ src/main/kotlin/io/suprgames/serverless/generator/ServerlessKDSLGenerator.kt
@@ -7,10 +7,11 @@ import org.reflections.scanners.SubTypesScanner
 import org.reflections.scanners.TypeAnnotationsScanner
 import org.reflections.util.ClasspathHelper
 import org.reflections.util.ConfigurationBuilder
+import java.lang.reflect.AnnotatedElement
+import java.lang.reflect.Method
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
-import kotlin.reflect.KFunction4
 
 class ServerlessKDSLGenerator(
         val baseFile: String = "serverless-base.yml",
@@ -34,7 +35,7 @@ class ServerlessKDSLGenerator(
                                 .setScanners(SubTypesScanner(), TypeAnnotationsScanner(), MethodAnnotationsScanner()))
 
         Files.write(
-                Paths.get("serverless.yml"),
+                Paths.get(serverlessFile),
                 Files.readAllLines(Paths.get(baseFile))
                         .toStringBuffer()
                         .appendln()
@@ -54,37 +55,107 @@ class ServerlessKDSLGenerator(
                 StandardOpenOption.TRUNCATE_EXISTING)
 
         println("************************************************************")
-        println("    File [serverless.yml] generated successfully")
+        println("    File [$serverlessFile] generated successfully")
         println("************************************************************")
+
+        Files.write(
+                Paths.get("js-serverless/$serverlessFile"),
+                Files.readAllLines(Paths.get("js-serverless/$baseFile"))
+                        .toStringBuffer()
+                        .appendln()
+                        .appendln("functions:")
+                        //Http functions are a bit different
+                        .append(httpFunctions(reflections, true))
+                        .append(generate(reflections, WebSocketConnector::class.java, WebSocketConnectorGenerator(), true))
+                        .append(generate(reflections, SqsConsumer::class.java, SqsConsumerGenerator(), true))
+                        .append(generate(reflections, EventBridgeListener::class.java, EventBridgeListenerGenerator(), true))
+                        .append(generate(reflections, AuthorizerFunction::class.java, AuthorizationFunctionGenerator(), true))
+                        .append(generate(reflections, CognitoUserPoolTriggered::class.java, CognitoGenerator(), true))
+                        .append(generate(reflections, MinionFunction::class.java, MinionFunctionGenerator(), true))
+                        .appendln()
+                        .appendln(signature())
+                        .toString()
+                        .toByteArray(),
+                StandardOpenOption.TRUNCATE_EXISTING)
+
+        println("*********************************************************************")
+        println("    File [js-serverless/$serverlessFile] generated successfully")
+        println("*********************************************************************")
     }
 
-    fun <T : Annotation> generate(reflections: Reflections, clazz: Class<T>, generator: Generator): StringBuffer = forClasses(reflections, clazz, generator).append(forMethods(reflections, clazz, generator))
+    fun <T : Annotation> generate(reflections: Reflections, clazz: Class<T>, generator: Generator, alterEgoPhase: Boolean = false): StringBuffer = forClasses(reflections, clazz, generator, alterEgoPhase).append(forMethods(reflections, clazz, generator, alterEgoPhase))
 
-    private fun <T : Annotation> forClasses(reflections: Reflections, clazz: Class<T>, generator: Generator): StringBuffer =
+    private fun <T : Annotation> forClasses(reflections: Reflections, clazz: Class<T>, generator: Generator, alterEgoPhase: Boolean): StringBuffer =
             reflections.getTypesAnnotatedWith(clazz)
                     .let { classesWithAnnotation ->
                         val stringBuffer = StringBuffer()
-                        println("      Generating [${classesWithAnnotation.size}] ${clazz.simpleName} for Class...")
+                        var processed = 0
                         classesWithAnnotation.forEach { annotatedClass ->
                             val annotation = annotatedClass.getAnnotation(clazz)
                             val defaultName = annotatedClass.simpleName
-                            val handlerName = annotatedClass.name
-                            stringBuffer.append(generator.generate(defaultName, handlerName, annotation))
+                            val handlerName = generateHandlerNameForClass(annotatedClass, alterEgoPhase)
+                            if (handlerName.isNotBlank()) {
+                                processed ++
+                                stringBuffer.append(generator.generate(defaultName, handlerName, annotation))
+                                stringBuffer.append(addRuntimeIfNeeded(annotatedClass, alterEgoPhase))
+                            }
                         }
+                        println("      Generating [$processed] ${clazz.simpleName} for Class...")
                         stringBuffer
                     }
 
-    private fun <T : Annotation> forMethods(reflections: Reflections, clazz: Class<T>, generator: Generator): StringBuffer =
+    /**
+     * If we have an alter ego for the method/class we will specify its Runtime
+     */
+    private fun addRuntimeIfNeeded(element: AnnotatedElement, alterEgoPhase: Boolean): StringBuffer {
+        val sb = StringBuffer()
+        if (!alterEgoPhase) {
+            val runtime: String? = (element.annotations.firstOrNull { it is AlterEgo } as? AlterEgo)?.runtime
+            if (runtime != null && runtime != AlterEgoRuntimes.NODEJS_12) {
+                sb.appendln("    runtime: $runtime")
+            }
+        }
+        return sb
+    }
+
+    private fun generateHandlerNameForClass(annotatedClass: Class<*>, alterEgoPhase: Boolean): String {
+        val alterEgo = (annotatedClass.annotations.firstOrNull { it is AlterEgo } as? AlterEgo)
+        return when {
+            alterEgoPhase && alterEgo != null && alterEgo.handlerPath.isNotBlank() -> alterEgo.handlerPath
+            alterEgoPhase && alterEgo != null -> "src/" + annotatedClass.name.split(".").joinToString("/") + ".handler"
+            !alterEgoPhase && alterEgo == null -> annotatedClass.name
+            else -> ""
+        }
+    }
+
+    private fun <T : Annotation> forMethods(reflections: Reflections, clazz: Class<T>, generator: Generator, alterEgoPhase: Boolean): StringBuffer =
             reflections.getMethodsAnnotatedWith(clazz)
                     .let { methodsWithAnnotation ->
                         val stringBuffer = StringBuffer()
-                        println("      Generating [${methodsWithAnnotation.size}] ${clazz.simpleName} for Methods...")
+                        var processed = 0
                         methodsWithAnnotation.forEach { annotatedMethod ->
                             val annotation = annotatedMethod.getAnnotation(clazz)
                             val defaultName = annotatedMethod.declaringClass.simpleName + "-" + annotatedMethod.name
-                            val handlerName = "${annotatedMethod.declaringClass.name}::${annotatedMethod.name}"
-                            stringBuffer.append(generator.generate(defaultName, handlerName, annotation))
+                            val handlerName = generateHandlerNameForMethod(annotatedMethod, alterEgoPhase)
+                            if (handlerName.isNotBlank()) {
+                                processed++
+                                stringBuffer.append(generator.generate(defaultName, handlerName, annotation))
+                                stringBuffer.append(addRuntimeIfNeeded(annotatedMethod, alterEgoPhase))
+                            }
                         }
+                        println("      Generating [${methodsWithAnnotation.size}] ${clazz.simpleName} for Methods...")
                         stringBuffer
                     }
+
+    private fun generateHandlerNameForMethod(annotatedMethod: Method, alterEgoPhase: Boolean): String {
+        val alterEgo = (annotatedMethod.annotations.firstOrNull { it is AlterEgo } as? AlterEgo)
+        return when {
+            alterEgoPhase && alterEgo != null && alterEgo.handlerPath.isNotBlank() -> alterEgo.handlerPath
+            alterEgoPhase && alterEgo != null -> "src/" + annotatedMethod.declaringClass.name.split(".").joinToString("/") + "." + annotatedMethod.name
+            !alterEgoPhase && alterEgo == null -> "${annotatedMethod.declaringClass.name}::${annotatedMethod.name}"
+            else -> ""
+        }
+    }
+
+
 }
